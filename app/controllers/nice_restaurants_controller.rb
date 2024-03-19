47ff36c9bb9b85a7f165a4674b783d955717afeb
hiroshi nagao <hi_1205@hotmail.com>
2024-03-19 21:19:47 +0900

    add more actions

--- app/controllers/nice_restaurants_controller.rb
+++ app/controllers/nice_restaurants_controller.rb
@@ -12,6 +12,30 @@ class NiceRestaurantsController < ApplicationController
   end
 
   def create
-    raise
+    @nice_restaurant = NiceRestaurant.new(nice_restaurant_params)
+    if @nice_restaurant.save
+      redirect_to nice_restaurant_path(@nice_restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @nice_restaurant = NiceRestaurant.find(params[:id])
+  end
+
+  def update
+    @nice_restaurant = NiceRestaurant.find(params[:id])
+    if @nice_restaurant.update(nice_restaurant_params)
+      redirect_to nice_restaurant_path(@nice_restaurant)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def nice_restaurant_params
+    params.require(:nice_restaurant).permit(:name, :address, :rating, :category)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/nice_restaurants/new', to: 'nice_restaurants#new', as: :new_nice_restaurant
   post '/nice_restaurants', to: 'nice_restaurants#create', as: :create_new_restaurant
   get '/nice_restaurants/:id', to: 'nice_restaurants#show', as: :nice_restaurant
+  get '/nice_restaurants/:id/edit', to: 'nice_restaurants#edit', as: :edit_nice_restaurant
+  patch '/nice_restaurants/:id', to: 'nice_restaurants#update'
 end
